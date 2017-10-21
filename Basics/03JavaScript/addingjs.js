0127d7fdc29eb1a1fd21b642ad0f1ab11d8db747
Linus Koepfer <linkoep@gmail.com>
2017-10-21 00:33:17 -0400

    Added basic html site, site with css, and site with css and javascript

---
+++ Basics/03JavaScript/addingjs.js
@@ -0,0 +1,7 @@
+console.log("Java Script can print messages to the console");
+window.alert("JavaScript can create popups");
+
+function printHelloWorld() {
+	document.getElementById("demo").innerHTML = "Hello World!";
+}
+
